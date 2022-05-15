38c9a35470b296ac066659813608a0fa3c75fe75
Abit Mahato <abitmahato712@gmail.com>
2022-05-15 07:34:54 +0545

    TODO app solution added

---
+++ app/src/main/java/com/example/todoapp/CreateInstanceActivity.java
@@ -0,0 +1,67 @@
+package com.example.todoapp;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CreateInstanceActivity extends AppCompatActivity {
+
+    public static final String NoteMeTitle = "com.example.notes.title";
+    public static final String NoteMeDescription = "com.example.notes.description";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#B6D3D0"));
+
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+        EditText Title = findViewById(R.id.notetitle);
+        EditText Description = findViewById(R.id.notedescription);
+        Button CreateInstance = findViewById(R.id.button);
+        Title.requestFocus();
+
+        Intent editIntent = getIntent();
+        if (editIntent.hasExtra("NoteMeUpdate")) {
+            setTitle("Update");
+            Title.setText(editIntent.getStringExtra("NoteMeTitle"));
+            Description.setText(editIntent.getStringExtra("NoteMeDescription"));
+
+        } else {
+            setTitle("Create");
+        }
+
+        CreateInstance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nTitle = Title.getText().toString().trim();
+                String nText = Description.getText().toString().trim();
+                Intent NewIntent = new Intent();
+
+                if (!nText.isEmpty()) {
+                    int id = getIntent().getIntExtra("NoteMeKey", -1);
+                    if (id != -1) NewIntent.putExtra("NoteMeKey", id);
+                    NewIntent.putExtra(NoteMeTitle, nTitle);
+                    NewIntent.putExtra(NoteMeDescription, nText);
+                    setResult(RESULT_OK, NewIntent);
+                    finish();
+                } else {
+                    Toast.makeText(CreateInstanceActivity.this, "Input field's cannot be empty!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+}
